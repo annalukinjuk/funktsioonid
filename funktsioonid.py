3a5bc571521b819c17e3765bdde664ae5c926977
opilane <opilane@HP-CZC6297BBM>
2021-11-10 11:29:19 +0200

    nnm

--- funktsioonid.py
+++ funktsioonid.py
@@ -1,3 +1,17 @@
-while True:
-    print("funktsioonid") center 
-def arithmetic 
+#while True:
+#    print("funktsioonid")
+
+def arithmetic(a: float,b:float,c:str):
+    """Lihtne kalkulaato
+    + - liitmine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param str c: Aritmeetiline tehing
+    :rtype float:
+    """
+    if c=="+":
+        r=a+b
+    elif c==
