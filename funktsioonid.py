6b7aa4f9366d77a788417aa6ec13d78424604495
opilane <opilane@HP-CZC6297BBM>
2021-11-12 11:21:35 +0200

    1

--- funktsioonid.py
+++ funktsioonid.py
@@ -4,18 +4,24 @@ from cmath import sqrt
 
 while True:
     print("funktsioonid".center(50, "+"))
-    v=input("arithmetic- A, is year leap - Y, check month season - C ")
+    v=input("arithmetic- A, is year leap - Y, check month season - C, is number prime - P, XOR - X  ")
     if v.upper()=="A":
         arv1=float(input("Arv 1ˇ:"))
         arv2=float(input("Arv 2ˇ:"))
         sym=input("tehe:")
         rezult=arithmetic(arv1,arv2,sym)
         print(rezult)
-    if v.upper()=="Y":
+    elif v.upper()=="Y":
         rezult=is_year_leap(int(input("siseta aasta: ")))
         print(rezult)
-    if v.upper()=="C":
+    elif v.upper()=="C":
         rezult=season(int(input("what month would you like to check?  ")))
         print(rezult)
-    if v.upper()=="P":
-        rezult=u
+    elif v.upper()=="P": 
+        rezult=is_prime(int(input("siseta number  ")))
+        print(rezult)
+    elif v.upper()=="X":
+        rezult=xor_cipher(input("siseta text"),input("siseta võti:"))
+        print(rezult)
+        de_rezult=xor_uncipher(rezult, input("siseta võti:"))
+        print(de_rezult)
--- module1.py
+++ module1.py
@@ -38,41 +38,56 @@ def is_year_leap(aasta: int):
     return vastus
 
 def season(seasondate: int):
-   if seasondate==12 or 1<= seasondate <=2:
+    """Sesooni leidimińe, tagastab nimi sesooni ja wrong number kui else
+    :param int seasonsdate: Sesooni date
+    :rtype var: funktsiooni vastus sesooni formadis
+    """
+    if seasondate==12 or 1<= seasondate <=2:
        print("winter")
-   elif  3<= seasondate <=5:
+    elif  3<= seasondate <=5:
        print("spring")
-   elif 6<= seasondate <=8:
+    elif 6<= seasondate <=8:
        print("summer")
-   elif 9<= seasondate <=11:
+    elif 9<= seasondate <=11:
        print("autunm")
-   else:
-       print("wrong number")
-   return seasondate
+    else:
+        print("wrong number")
+    return seasondate
 
-from cmath import sqrt
+def is_prime(number: int):
+    """priim numbri leidmine
+    Tagastab true kui number on kerge, tahastab false kui élse.
+    :param int number: number
+    :rtype bool: Funktsiooni vastus logilises formaadis
+    """
+    if number %2==0 and number !=2:
+        return False
+    if number==0 or number==1:
+        return False
+    for number in range(3,int(sqrt(number).real) +1,2):
+        if number %n==0:
+            return False
+    return True
 
-
-#def prime(number):
-#    if number %2==0 and number !=2:
-#        return False
-#    if number==0 or number==1:
-#        return False
-#    for n in range(3,int(sqrt(number).real) +1,2):
-#        if number %n==0:
-#            return False
-#    return True
-
-#n = int(input("siseta number  "))
-#print(prime(n))
-
-import datetime
-d=int(input("Введите день"))# предположим - 29
-m=int(input("Введите месяц"))# предположим - 2
-y=int(input("Введите год"))# предположим - 2020
-try:
-    data=datetime.date(y,m,d)
-    print(data)
-    print("Дата существующая")
-except:
-    print("Такой даты нет")
+def xor_cipher(string: str, key:str)->str:
+    """Tavaline sõna kodeeritakse
+    """
+    result=''
+    temp=int()
+    for i in range(len(string)):
+        temp=ord(string[i]) #näitab sümboli kood
+        for j in range(len(key)):
+            temp^=ord(key[j])
+        result+=chr(temp)
+    return result
+def xor_uncipher(string: str, key: str)->str:
+    """kodeeritud text dekoreeritakse
+    """
+    result=''
+    temp=[]
+    for i in range(len(string)):
+        temp.append(string[i])
+        for j in reversed(range(len(key))):
+            temp[i]=chr(ord(key[j]))^ord(temp[i])
+        result+=temp[i]
+    return result
